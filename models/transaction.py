14afd3ab45d62131228591fb05abd342160285ca
Briana Vecchione <briana.vecchione@gmail.com>
2016-02-20 19:31:21 -0500

    changed vacation_listing.py to transaction.py

--- models/transaction.py
+++ models/transaction.py
@@ -1,6 +1,6 @@
 
-class VacationListing(db.Model):
-    __tablename__ = "vacation_listings"
+class Transaction(db.Model):
+    __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
     starting_location = db.Column(db.String, nullable=False)
@@ -9,8 +9,8 @@ class VacationListing(db.Model):
     departure_date = db.Column(db.Integer, nullable=False)
 
     listing_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    listing = db.relationship("User", back_populates="vacation_listings")
-    reservations = db.relationship("Reservation", back_populates="vacation_listing")
+    listing = db.relationship("User", back_populates="transactions")
+    reservations = db.relationship("Reservation", back_populates="transaction")
 
     def __init__(self, starting_location, destination, cost, departure_date, listing):
         self.starting_location = starting_location
@@ -20,4 +20,5 @@ class VacationListing(db.Model):
         self.listing = listing
 
     def __repr__(self):
-        return '<VacationListing %r %r %r %r>' % self.id, self.destination, self.cost, self.departure_date
+        return '<Transaction %r %r %r %r>' % self.id, self.destination, self.cost, self.departure_date
+

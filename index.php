b3a0622d129b129f924c571f00c0554ce025f454
Kathryn Hodge <kahodge@vassar.edu>
2016-02-20 20:04:48 -0500

    Cleaning Front-End

--- index.php
+++ index.php
@@ -1,36 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kathrynhodge
- * Date: 2/20/16
- * Time: 3:06 AM
- * php-intro.com
-*/
+//include our settings, connect to database etc.
+//include dirname($_SERVER['DOCUMENT_ROOT']).'/cfg/settings.php';
+//getting required data
+//$DATA=dbgetarr("SELECT * FROM links");
+//$pagetitle = "Travel Broke";
+//etc
+//and then call a template:
+//$tpl = "layouts/base.php";
+include "layouts/base.php";
 ?>
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>TRAVEL-BROKE</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/assets.css">
-        <link rel="stylesheet" href="css/font-awesome.css">
-        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'  type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.js"></script>
-        <script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
-    </head>
-
     <body>
-        <script>
-            var myDataRef = new Firebase('https://incandescent-fire-1314.firebaseio.com/');
-
-        </script>
-
     <div class="container-fluid" id="bg">
         <section id="pd50">
 
--- pickTravel.php
+++ pickTravel.php
@@ -5,25 +5,9 @@
  * Date: 2/20/16
  * Time: 10:55 AM
  */
+include "layouts/base.php";
 ?>
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>TRAVEL-BROKE</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/assets.css">
-    <link rel="stylesheet" href="css/font-awesome.css">
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'  type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
-
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</head>
 <body>
 
 <div class="container-fluid" id="bg">
